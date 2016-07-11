8d54dc00d21d0965a9e0318a75663671c24c07b2
jarrancarr <jarran.carr@yahoo.com>
2016-07-11 15:50:40 -0400

    working cookies

--- src/senSavories.go
+++ src/senSavories.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	//"crypto/md5"
-	"fmt"
+	//"fmt"
 	//"io"
 	//"io/ioutil"
 	"net/http"
-	"os"
+	//"os"
 	//"strconv"
 	//"html/template"
 	//"strings"
@@ -32,6 +32,7 @@ func main() {
 }
 
 func setup() {
+	//website
 	senSavories = website.CreateSite("senSavories")
 	senSavories.AddMenu("nav").
 		AddItem(&html.HTMLMenuItem{"/test", "Test", html.HTMLElement{}}).
@@ -42,37 +43,29 @@ func setup() {
 		AddItem(&html.HTMLMenuItem{"/login", "login", html.HTMLElement{}}).
 		Add("nav nav-pills nav-stacked", "", "")
 
+	acs := website.CreateAccountService()
+	senSavories.AddService("account", acs)
 	senSavories.AddService("message", service.CreateMessageService())
-	secure := service.CreateAccountService()
-	senSavories.AddService("account", secure)
-	head := addPage(senSavories, "", "head", "/head")
-	senSavories.AddPage("head", head)
-	nav := addPage(senSavories, "nav", "nav", "")
-	senSavories.AddPage("nav", nav)
 
-	main := addPage(senSavories, "senSavories", "main", "/")
+	// template subpages
+	senSavories.AddPage("head", "head", "")
+	senSavories.AddPage("nav", "nav", "")
+
+	// pages
+	main := senSavories.AddPage("senSavories", "main", "/")
 	main.AddTable("cart", []string{"A", "B", "C", "D"}, []string{"1", "2", "3", "4"}).AddClass("table")
-	addPage(senSavories, "Home", "home", "/home")
-	addPage(senSavories, "senSavories-edit", "edit", "/edit")
-	addPage(senSavories, "senSavories", "test", "/test")
-	addPage(senSavories, "Home", "home", "/secure").AddInitProcessor(secure.ValidateSession)
-	addPage(senSavories, "message", "message", "/message")
-	addPage(senSavories, "", "login", "/login").AddPostHandler("login", secure.LoginPostHandler)
+	senSavories.AddPage("Home", "home", "/home")
+	senSavories.AddPage("senSavories-edit", "edit", "/edit")
+	senSavories.AddPage("senSavories", "test", "/test")
+	senSavories.AddPage("message", "message", "/message")
+	login := senSavories.AddPage("login", "login", "/login")
+	login.AddPostHandler("login", acs.LoginPostHandler)
 
-	Shelf = []ecommerse.Category{
-		ecommerse.Category{"Oils", "Olive Oils", "oils.png"},
-		ecommerse.Category{"Vinegars", "Aged Balsamic Vinegars", "vinegars.png"},
-		ecommerse.Category{"Spices", "African Spices", "spices.png"},
-		ecommerse.Category{"Teas", "Quality East African Teas", "teas.png"},
-		ecommerse.Category{"Coffee", "Coffee from Africa", "coffee.png"},
-	}
-}
-
-func addPage(site *website.Site, name, template, url string) *website.Page {
-	page, err := website.LoadPage(site, name, template, url)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	return page
+	//	Shelf = []ecommerse.Category{
+	//		ecommerse.Category{"Oils", "Olive Oils", "oils.png"},
+	//		ecommerse.Category{"Vinegars", "Aged Balsamic Vinegars", "vinegars.png"},
+	//		ecommerse.Category{"Spices", "African Spices", "spices.png"},
+	//		ecommerse.Category{"Teas", "Quality East African Teas", "teas.png"},
+	//		ecommerse.Category{"Coffee", "Coffee from Africa", "coffee.png"},
+	//	}
 }
